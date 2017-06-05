89d9a6e3dca0f35eb235b97965f4a5bf76069c22
JodyLeSage <jody.c.lesage@gmail.com>
2017-06-04 22:17:43 -0400

    project stabalized

---
+++ jodylesage.js
@@ -0,0 +1,14 @@
+/* custom js for jodylesage.com */
+
+/* initialize the page */
+/*
+$(document).ready(function(){
+	addListeners();
+});
+*/
+/* function for adding listers to page elements */
+/*
+function addListeners(){
+
+}
+*/
