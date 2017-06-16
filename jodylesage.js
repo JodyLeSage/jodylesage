dae7498b2a315de0aa0892c6b5c57eb950ee7ef1
JodyLeSage <jody.c.lesage@gmail.com>
2017-06-16 16:27:01 -0400

    navbar implemented and resume added

--- jodylesage.js
+++ jodylesage.js
@@ -1,14 +1,21 @@
 /* custom js for jodylesage.com */
 
 /* initialize the page */
-/*
 $(document).ready(function(){
 	addListeners();
 });
-*/
-/* function for adding listers to page elements */
-/*
-function addListeners(){
 
+/* function for adding listers to page elements */
+function addListeners(){
+	/*
+		jQuery for navbar buttons
+		https://stackoverflow.com/questions/19579083/bootstrap-how-to-use-navbar
+	*/
+	$(".navbar-nav a").on('click',function(e) {
+		e.preventDefault(); // stops link form loading
+		$('.content').hide(); // hides all content divs
+		$('#' + $(this).attr('href') ).show(); //get the href and use it find which div to show
+	});
 }
-*/
+
+
