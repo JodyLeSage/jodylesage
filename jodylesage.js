25fd76d4227150081f65a0f3a307faec66c0338f
JodyLeSage <jody.c.lesage@gmail.com>
2017-06-17 04:35:16 -0400

    recaptcha finished

--- jodylesage.js
+++ jodylesage.js
@@ -1,4 +1,5 @@
 /* custom js for jodylesage.com */
+var userIsValidated = false;
 
 /* initialize the page */
 $(document).ready(function(){
@@ -17,11 +18,55 @@ function addListeners(){
 		$('#' + $(this).attr('href') ).show(); //get the href and use it find which div to show
 	});
 	
-	// validate user after page loads
-	$(window).on('load', function(){});
+	// validate user when viewing private data
+	$('.contains-private-info').one('click', function(){
+		if(!userIsValidated){
+			grecaptcha.execute();
+		}
+	});
 }
 
-/* code that runs after user is validated using invisible reCAPTCHA */
-function userValidated(token){
-	console.log("USER VALIDATED");
+/* use the backend to verify the user was indeed validated by recaptcha */
+function userValidated(token) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			var json = JSON.parse(this.responseText);
+			populatePersonalData(json);
+		}
+	};
+	xmlhttp.open("POST", "php/validateUser.php?token=" + token, true);
+	xmlhttp.send();
+}
+
+/* tells recaptcha how to behave */
+function onloadRecaptchaCallback(){
+	grecaptcha.render('submitRecaptcha', {
+		'sitekey' : '6LeOwyUUAAAAAIy-WfjdNkxv-KcWauJ6mzSiICUH',
+		'callback' : userValidated
+	});
+}
+
+/* function for inserting personal data into the site after the user has been validated */
+function populatePersonalData(json){
+	// update the email anchor in the resume iFrame
+	var iframeBody = $('#resume-iframe').contents().find('body');
+	var emailItem = iframeBody.contents().find('#resume-email');
+	if(json["email"]){
+		emailItem.attr({
+			"href" : "mailto:" + json["email"],
+			"itemprop" : "email"
+		});
+		emailItem.hide().html(json["email"]).fadeIn('slow');	// fade in for dramatic effect
+	}
+	
+	// update information on the contact page
+	// cannot be streamlined because of the iFrame
+	emailItem = $('#contact-email');
+	emailItem.attr({
+		"href" : "mailto:" + json["email"],
+		"itemprop" : "email"
+	});
+	emailItem.hide().html(json["email"]).fadeIn('slow');
+	
 }
---
+++ php/validateUser.php
@@ -0,0 +1,32 @@
+<?php
+	// https://stackoverflow.com/questions/5647461/how-do-i-send-a-post-request-with-php
+
+	require 'privateData.php';
+	
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$data = array('secret' => $privateRecaptchaKey, 'response' => $_REQUEST["token"]);
+	
+	$options = array(
+		'http' => array(
+			'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method' => 'POST',
+			'content' => http_build_query($data)
+		)
+	);
+	$context = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	
+	if ($result === FALSE) {
+		// TODO: handle errors
+	}
+	
+	$data = json_decode($result, true);
+	$success = $data["success"];
+	
+	if ($success) {
+		echo json_encode($realData, JSON_UNESCAPED_SLASHES);
+	}
+	else {
+		echo json_encode($fakeData, JSON_UNESCAPED_SLASHES);
+	}
+?>
