74b290d6af96462f6245d59964cbbc63f57c5358
JodyLeSage <jody.c.lesage@gmail.com>
2017-09-09 19:11:26 -0400

    typo fixed

--- js/jodylesage.js
+++ js/jodylesage.js
@@ -13,7 +13,7 @@ function addListeners(){
 		https://stackoverflow.com/questions/19579083/bootstrap-how-to-use-navbar
 	*/
 	$(".navbar-nav a").on('click',function(e) {
-		e.preventDefault(); // stops link frmm loading
+		e.preventDefault(); // stops link from loading
 		$('.content').hide(); // hides all content divs
 		$('#' + $(this).attr('href') ).show(); //get the href and use it find which div to show
 	});
