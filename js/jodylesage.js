dfd65eb8bc9fd79802896f9231c548fb42520791
JodyLeSage <jody.c.lesage@gmail.com>
2017-09-09 15:35:51 -0400

    clicking brant item goes to home

--- js/jodylesage.js
+++ js/jodylesage.js
@@ -13,14 +13,13 @@ function addListeners(){
 		https://stackoverflow.com/questions/19579083/bootstrap-how-to-use-navbar
 	*/
 	$(".navbar-nav a").on('click',function(e) {
-		e.preventDefault(); // stops link form loading
+		e.preventDefault(); // stops link frmm loading
 		$('.content').hide(); // hides all content divs
 		$('#' + $(this).attr('href') ).show(); //get the href and use it find which div to show
 	});
 	
-	// validate user when viewing private data
-	$(window).on('load', function(){
-		
+	$(".navbar-brand").on('click', function(){
+		$("#homeTab").click();
 	});
 }
 
