4508847dacb1916419a115cfddb7f292f60982c7
JodyLeSage <jody.c.lesage@gmail.com>
2017-11-15 22:03:27 -0500

    Proofreading, added certification section

--- js/helperfunctions.js
+++ js/helperfunctions.js
@@ -2,14 +2,22 @@
 function getCurrentYearAndMonth() {
 	var d = new Date();
 	var s = d.getFullYear() + '-';
-	if (d.getMonth() < 10){
+	if (d.getMonth() < 9){
 		s = s + '0';	// pad a leading zero for single-digit months
 	}
-	var s = s + d.getMonth();
+	var s = s + (d.getMonth() + 1);
 	return s;
 }
 
 // prints the resume when the print resume button is clicked
 function printResume(){
 	window.print();
+};
+
+// populate microdata with current month
+window.onload = function() {
+	var x = document.getElementsByClassName("present");
+    for (var i = 0; i < x.length; i++) {
+        x[i].setAttribute("datetime",getCurrentYearAndMonth());
+    }    
 };
